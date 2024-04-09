073bcfb60274b5c47aa12e7c688bf98d47d4c596
sampolucas88@gmail.com <sampolucas88@gmail.com>
2024-04-09 19:15:40 -0300

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryJuegoSampo
+{
+    public partial class Form1 : Form
+    {
+        //zona de decalracion global
+        clsNave objNaveJugador;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            objNaveJugador = new clsNave();
+
+            objNaveJugador.CrearJuagador();
+            objNaveJugador.imgNave.Location = new Point(200, 200);
+            Controls.Add(objNaveJugador.imgNave);
+
+            //MessageBox.Show(objNaveJugador.nombre);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Right)
+            {
+                objNaveJugador.imgNave.Location = new Point(
+                    objNaveJugador.imgNave.Location.X + 5, 
+                    objNaveJugador.imgNave.Location.Y);
+            }
+
+            if (e.KeyCode == Keys.Left)
+            {
+                objNaveJugador.imgNave.Location = new Point(
+                    objNaveJugador.imgNave.Location.X - 5,
+                    objNaveJugador.imgNave.Location.Y);
+            }
+        }
+    }
+}
---
+++ clsNave.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryJuegoSampo
+{
+    internal class clsNave
+    {
+        //Caracteristicas
+        public int vida;
+        public string nombre;
+        int puntosDaño;
+        public PictureBox imgNave; 
+
+        public void CrearJuagador()
+        {
+            vida = 100;
+            nombre = "Jugador1";
+            puntosDaño = 1;
+            imgNave = new PictureBox();
+            imgNave.SizeMode = PictureBoxSizeMode.StretchImage;
+            imgNave.ImageLocation = "https://cdna.artstation.com/p/assets/images/images/053/627/736/large/otis-jackson-iv-galaga1-512x512-copy.jpg?1662646025";
+        }
+
+        public void CrearEnemigo()
+        {
+            vida = 25;
+            nombre = "malo1";
+            puntosDaño = 2;
+        }
+    }
+}
